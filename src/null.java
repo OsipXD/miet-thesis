73b8942ad0f7ae7769e7784c082ac12354e285c5
OsipXD <osip.fatkullin@gmail.com>
2018-05-28 16:13:51 +0300

    Добавлена схема алгоритма
    А так же:
    - Файл схемы данных
    - Пример null-безопасного кода на Java

---
+++ src/null.java
@@ -0,0 +1,19 @@
+public class Foo {
+    private final String bar;
+
+    public Foo(String bar) {
+        this.bar = bar;
+    }
+
+    public String getBar() {
+        return bar;
+    }
+}
+
+// Допустим, что метод getFoo может вернуть объект Foo или null
+Foo buz = getFoo();
+
+int length = 0;
+if (buzz != null && buzz.getBar() != null) {
+    length = buz.getBar().length;
+}
