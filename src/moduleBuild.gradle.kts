f632d5876a9a90a2e0208b1164fac325cf280eab
OsipXD <osip.fatkullin@gmail.com>
2018-06-04 15:51:52 +0300

    Введение в Gradle

---
+++ src/moduleBuild.gradle.kts
@@ -0,0 +1,13 @@
+android {
+    compileSdkVersion(27)
+    buildToolsVersion("27.0.3")
+
+    defaultConfig {
+        minSdkVersion(19)
+        targetSdkVersion(27)
+
+        applicationId = "ru.endlesscode.miet.orioks"
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
