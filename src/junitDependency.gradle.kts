a290a1ad6694275919622a821fb2dc0d3a96a0b6
OsipXD <osip.fatkullin@gmail.com>
2018-06-05 14:17:17 +0300

    Объявление зависимостей

---
+++ src/junitDependency.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    java
+}
+
+repositories {
+    mavenCentral() // Подключение репозитория |\label{line:repo}|
+}
+
+dependencies {
+    testImplementation("junit:junit:4.12") // Подключение зависимости |\label{line:junit}|
+}
